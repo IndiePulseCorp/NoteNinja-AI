b7a8dd23d035c5319c817b5f3ba4b95269676006
Robert <indiepulsecorp@gmail.com>
2024-09-02 21:43:12 -0500

    Initial Commit

---
+++ NoteNinja/NoteNinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteNinjaApp.swift
+//  NoteNinja
+//
+//  Created by Robert Greenwood on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteNinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
